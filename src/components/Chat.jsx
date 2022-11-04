e337fcf196b861b79e9979db4f39b45fa898061e
tona3922 <vothaitoan12@gmail.com>
2022-11-04 23:54:09 +0700

    ""

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,12 +3,15 @@ import More from "../img/option.png";
 import Video from "../img/video.png";
 import { Messages } from "./Messages";
 import { Input } from "./Input";
+import ava from "../img/avatar.jpg";
 
 export const Chat = () => {
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Jane</span>
+        <span>
+          <img src={ava} alt="" /> Jane
+        </span>
         <div className="chatIcons">
           <img src={Video} alt="" />
           <img src={More} alt="" />
