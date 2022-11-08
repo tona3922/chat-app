39de35b2500bd1280d88f4cb3e3662099d631488
hikaric <75846355+hikaric@users.noreply.github.com>
2022-11-08 21:20:15 +0700

    Update Register.jsx
    thêm lưu ava vào db

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Add from "../img/add-photo.png";
 import backgr from "../img/background.jpg";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { async } from "@firebase/util";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 // import { login } from "../pages/Login";
 export const Register = () => {
-  const handleSubmit = (e)=>{
+  const [err, seterr] = useState(false);
+  const handleSubmit = async (e)=>{
     e.preventDefault()
+    //console.log(e.target[0].value);
     const dname = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    const ava = e.target[3].files[0];
+    const file = e.target[3].files[0];
 
     // chèn từ link firebase nhớ xóa getAuth
     //import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
     //const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+    try{
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      /*.then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
         console.log(user);
@@ -27,7 +32,49 @@ export const Register = () => {
         const errorCode = error.code;
         const errorMessage = error.message;
         // ..
-      });
+      });*/
+
+      //const storage = getStorage();
+      const storageRef = ref(storage, dname);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed', 
+      /*  (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        }, */
+        (error) => {
+          // Handle unsuccessful uploads
+          seterr(true);
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+           // console.log('File available at', downloadURL);
+            await updateProfile(res.user,{dname, photoURL: downloadURL});
+          });
+        }
+      );
+    }
+    catch(err){
+        seterr(true);
+    }
   }
 
   return (
@@ -52,8 +99,9 @@ export const Register = () => {
             <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
+          {err &&<span>Please retry (longer password maybe?)</span>}
         </form>
-        {/* Already have an account ? <Link to="/login">Login</Link> */}
+        {/*<span>Already have an account ?</span> <Link to="/login">Login</Link>*/}
       </div>
     </div>
   );
