6abd6bd9cc8eadc3094d6a47603035846f9dcb4b
hikaric <75846355+hikaric@users.noreply.github.com>
2022-11-08 19:33:59 +0700

    Update firebase.js
    đổi sang fb của t

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import{getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArdzVqvqJCqQrQGPo93wZVCekqxTgDgAk",
-  authDomain: "chat-47028.firebaseapp.com",
-  projectId: "chat-47028",
-  storageBucket: "chat-47028.appspot.com",
-  messagingSenderId: "740041054895",
-  appId: "1:740041054895:web:f545c9296d1c68210f125a",
+  apiKey: "AIzaSyA6bwZZVw_Qea2T53-JyJZklKp0_3oYK30",
+  authDomain: "chat-app-ee423.firebaseapp.com",
+  projectId: "chat-app-ee423",
+  storageBucket: "chat-app-ee423.appspot.com",
+  messagingSenderId: "239230330029",
+  appId: "1:239230330029:web:919989f1433c915c2f1224"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-export const storage = getStorage();
-export const db = getFirestore(app);
