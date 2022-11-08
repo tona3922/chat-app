17dea8bab4878e2340f71943bd3cc7d9b4827b71
hikaric <75846355+hikaric@users.noreply.github.com>
2022-11-08 19:35:04 +0700

    Update Register.jsx
    làm backend của đăng ký
    đang bug là chưa gửi đi đc

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import Add from "../img/add-photo.png";
 import backgr from "../img/background.jpg";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 // import { login } from "../pages/Login";
 export const Register = () => {
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    const dname = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const ava = e.target[3].files[0];
+
+    // chèn từ link firebase nhớ xóa getAuth
+    //import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+    //const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
   return (
     <div
       className="formContainer"
@@ -15,8 +42,8 @@ export const Register = () => {
       <div className="formWrapper">
         <span className="logo">Goszip</span>
         <span className="title">Register</span>
-        <form>
-          <input type="text" placeholder="Your name" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Your display name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <input style={{ display: "none" }} type="file" id="file" />
