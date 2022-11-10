8c22ad3264c56ab8bbb524913bb2cb4e259c8bb4
hikaric <75846355+hikaric@users.noreply.github.com>
2022-11-10 15:57:15 +0700

    Update Login.jsx
    làm login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import backgr from "../img/background.jpg";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../firebase"
+import { Navigate } from "react-router";
 
 export const Login = () => {
+
+  const [err, seterr] = useState(false);
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    try{
+      await signInWithEmailAndPassword(auth, email, password);
+      Navigate("/login")
+    }
+    catch(err){
+        seterr(true);
+    }
+  };
+
   return (
     // <div>Hello</div>
     <div
@@ -15,10 +33,11 @@ export const Login = () => {
       <div className="formWrapper">
         <span className="logo">Goszip</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <button>Sign in</button>
+          {err && <span>Please retry (longer password maybe?)</span>}
         </form>
         <p>
           {/* Don't have an account? <Link to="/register">Register</Link> */}
