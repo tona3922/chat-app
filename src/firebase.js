80c4f07576df6c82823514cd298f60ea5585df04
hikaric <75846355+hikaric@users.noreply.github.com>
2022-11-10 15:18:17 +0700

    Update firebase.js
    thêm thư viện để lưu thông tin người dùng

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import{getAuth} from "firebase/auth"
+import{getAuth} from "firebase/auth";
+import { getDoc, getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
